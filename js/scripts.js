5bd45a1fdb6cb578b487db9cc3792ea2b7f42fb4
kuhriboh <mikelguwani@gmail.com>
2021-08-12 15:27:32 -0400

    button
    changes code in js, index, and css files and adds buttons

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,46 @@ let pokemonRepository = (function() {
     {name: 'Lugia', height:205.2, types: ['Psychic', 'Flying'] },
     {name: 'Dragonite', height:87.6, types: ['Dragon', 'Flying'] },
     {name: 'Piplup', height:16.8, types: 'Water' }
-    
-  ]
-}
+
+  ];
+    // Functions Add Item And Buttons to Pokedex
+    function addListItem(pokemon){
+      let list = document.querySelector('.pokemon-list');
+      let listItem = document.createElement ('li');
+      let button = document.createElement('button');
+      button.innerText = pokemon.name;
+      button.classList.add('fancyButton');
+      listItem.appendChild(button);
+      list.appendChild(listItem);
+      button.addEventListener('click', function(event){
+        showDetails(pokemon);
+      });
+    }
+
+    function showDetails(pokemon){
+      console.log(pokemon);
+    }
+
+    function add(pokemon){
+      pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+      return pokemonList;
+    }
+
+    function removeLast() {
+      pokemonList.pop();
+    }
+
+    return {
+      add: add,
+      getAll: getAll,
+      removeLast:removeLast,
+      addListItem: addListItem
+    };
+}) ();
+
+pokemonRepository.getAll().forEach(function(pokemon){
+pokemonRepository.addListItem(pokemon);
+})
