dc97cdf027e3432e596e83062a2ee56774ce4887
kuhriboh <83624365+kuhriboh@users.noreply.github.com>
2021-08-12 14:35:08 -0400

    Update scripts.js
    changes add pokemon list to the website

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-alert('Hello world');
-let favoriteFood ='Pineapple';
-document.write(favoriteFood);
+let pokemonRepository = (function() {
+  let pokemonList = [
+    // height = inches
+    {name: 'Garchmop', height:75.6, types: ['Dragon','Ground'] },
+    {name: 'Celebi', height:24, types: ['Psychic', 'Grass'] },
+    {name: 'Mew', height:16.8, types:'Psychic' },
+    {name: 'Blaziken', height:75.6, types: ['Fire','Fighting'] },
+    {name: 'Lugia', height:205.2, types: ['Psychic', 'Flying'] },
+    {name: 'Dragonite', height:87.6, types: ['Dragon', 'Flying'] },
+    {name: 'Piplup', height:16.8, types: 'Water' }
+    
+  ]
+}
