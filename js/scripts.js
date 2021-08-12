6d2fbe12fa4eec984e39d0d4794e142b91eeb87f
kuhriboh <83624365+kuhriboh@users.noreply.github.com>
2021-08-12 01:14:52 -0400

    Update scripts.js
    change code

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>Simple JS App</title>
-    <link href="style.css" rel="stylesheet" type="text/css" />
-  </head>
-  <body>
-    <script>alert('Hello world');</script>
-  </body>
-</html>
+alert('Hello world');
