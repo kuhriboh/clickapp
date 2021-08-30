997aae3f639f07391ab06ffdef844b73c5ff1d2f
kuhriboh <mikelguwani@gmail.com>
2021-08-30 11:31:27 -0400

    added modal
    add code to add a modal that shows info about pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function() {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
     //modal
   let pokedexpokemonList = document.querySelector('.pokemon-list');
   let pokedexScreen = document.querySelector('.pokedex-screen');
@@ -26,7 +26,7 @@ let pokemonRepository = (function() {
       let list = document.querySelector('.pokemon-list');
       let listItem = document.createElement ('li');
       let button = document.createElement('button');
-      button.innerText = pokemon.name;
+      button.innerText = pokemon.name.toUpperCase();
       button.classList.add('fancyButton');
       listItem.appendChild(button);
       list.appendChild(listItem);
@@ -47,31 +47,33 @@ let pokemonRepository = (function() {
     }
 
     function loadList() {
-      return fetch(apiUrl).then(function (response) {
-        return response.json();
-      }).then(function (json) {
-        json.results.forEach(function (item) {
-          let pokemon = {
-            name: item.name,
-            detailsUrl: item.url
-          };
-          add(pokemon);
-          console.log(pokemon);
-        });
-      }).catch(function (e) {
+        return fetch(apiUrl)
+        .then(response => response.json())
+        .then(json => {
+            json.results.forEach(function (item) {
+              let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+              add(pokemon);
+              console.log(pokemon);
+              });
+        })
+        .catch(function (e) {
           console.error(e);
         })
-    }
+      }
 
     function loadDetails(item) {
       let url = item.detailsUrl;
-      return fetch(url).then(function (response) {
+      return fetch(url)
+      .then(function (response) {
         return response.json();
       }).then(function (details) {
     // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
-        item.types = details.types;
+        item.types = details.types[0].type.name;
       }).catch(function (e) {
         console.error(e);
       });
